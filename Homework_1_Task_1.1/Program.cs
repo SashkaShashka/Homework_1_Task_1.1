be4a4d171837d1f77f1740fe4ed58712fb839e2e
SashkaShashka <sasha151299qwe@gmail.com>
2021-10-04 12:45:03 +0400

    Добавьте файлы проекта.

---
+++ Homework_1_Task_1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework_1_Task_1._1
+{
+    class Program
+    {
+        static long GetSqr(long num)
+        {
+            return num * num;
+        }
+
+        static void Main(string[] args)
+        {
+            long num;
+            Console.Write("Введите число: ");
+
+            while (!long.TryParse(Console.ReadLine(), out num) || num < -1_000_000 || num > 1_000_000)
+            {
+                Console.Error.WriteLine("Что-то пошло не так, повторите попытку!");
+                Console.Write("Введите число: ");
+            }
+
+            Console.WriteLine("Квадрат " + num + " равен: " + GetSqr(num));
+        }
+
+
+    }
+}
